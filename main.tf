c930409542b444cb438cb99c33ad9aa50b19eaf1
Andy Ly <andy.ly@sainsburys.co.uk>
2022-10-31 11:58:30 +0000

    changed region to eu-west-2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "eu-west-3"
+    region = "eu-west-2"
 }
 
 resource "aws_sns_topic" "my_first_sns_topic" {
