d26209f4dcd43b4040847da9f968bde5ee6a6d22
Andy Ly <andy.ly@sainsburys.co.uk>
2022-10-31 14:27:07 +0000

    added sqs service

---
+++ main.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "eu-west-2"
+}
+
+resource "aws_sns_topic" "my_first_sns_topic" {
+  name = var.sns_name
+}
+
+resource "aws_sns_topic_policy" "my_sns_topic_policy" {
+  arn = aws_sns_topic.my_first_sns_topic.arn
+  policy = data.aws_iam_policy_document.my_custom_sns_policy_document.json
+}
+
+data "aws_iam_policy_document" "my_custom_sns_policy_document" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.my_first_sns_topic.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
+
+resource "aws_sqs_queue" "my_first_sqs" {
+  name                      = var.sqs_name
+}
+
+resource "aws_sqs_queue_policy" "my_sqs_policy" {
+  queue_url = aws_sqs_queue.my_first_sqs.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.my_first_sqs.arn}"
+    }
+  ]
+}
+POLICY
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "access_key" {
+        description = "Access key of AWS IAM user"
+}
+variable "secret_key" {
+        description = "Secret key of AWS IAM user"
+}
+
+
+variable "sns_name" {
+        description = "Name of the SNS Topic to be created"
+        default = "my_first_sns"
+}
+
+variable "sqs_name" {
+        description = "Name of the sqs queue to be created. You can assign any unique name for the Queue"
+        default = "my-first-sqs"
+}
+
+variable "account_id" {
+        description = "My Account Number"
+        default = "191954872959"
+}
