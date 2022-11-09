0a1466084a570ef05cbfdf67c7655b3fc51cbef1
Andy Ly <andy.ly@sainsburys.co.uk>
2022-11-09 11:54:44 +0000

    reinstated provide, removed secret key args

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-# provider "aws" {
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-#   region     = "eu-west-2"
-# }
+provider "aws" {
+  region     = "eu-west-2"
+}
 
 # The configuration for the `remote` backend.
 terraform {
