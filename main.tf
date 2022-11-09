d114cbb4b801c08aaf00b6b85a2a97077d0a1b86
Andy Ly <andy.ly@sainsburys.co.uk>
2022-11-09 11:35:26 +0000

    hardcoded region

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     = var.region
+  region     = "eu-west-2"
 }
 
 # The configuration for the `remote` backend.
